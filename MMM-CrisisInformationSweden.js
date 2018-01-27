b770ffb12199c445697655f242e742b731142a48
Anders Boghammar (Fang) <anders@boghammar.com>
2018-01-27 20:08:39 +0100

    Fixed bug #3 handling oldest correct. Also fixxed area == null problem Added some debug output Cleaned up readme

--- MMM-CrisisInformationSweden.js
+++ MMM-CrisisInformationSweden.js
@@ -19,13 +19,13 @@ Module.register("MMM-CrisisInformationSweden", {
     // --------------------------------------- Define module defaults
     defaults: {
         alwaysNational: true,           // Optional, Regardless of other settings always show national info. Not implemented yet
-        updateInterval: 10*60*1000,     // Optional. Number of ms between API updates. Not implemented yet.
-        uiUpdateInterval: 10*1000,      // Optional. Number of ms between changing to next announcement. Not implemented yet.
+        updateInterval: 30*60*1000,     // Optional. Number of ms between API updates. 
+        uiUpdateInterval: 10*1000,      // Optional. Number of ms between changing to next announcement. 
         areas: [],                      // Optional. An array of strings with area names. 
                                         // Only those messages aimed at the areas listed in the array are shown. 
                                         // If empty or undefined show all messages. Not implemented yet.
         showDescription: true,          // Optional. Show message description. Not yet implemented.
-        oldest: 7,                      // Optional. Dont show messages older then this number of days. Not yet implemented.
+        oldest: 7,                      // Optional. Dont show messages older then this number of days.
     },
     
     // --------------------------------------- Define required scripts
@@ -64,55 +64,80 @@ Module.register("MMM-CrisisInformationSweden", {
         // ------ Display a selected message in the feed
         if (this.currentFeedIndex >= this.currentFeed.length) this.currentFeedIndex = 0;
         if (this.currentFeed.length > 0) { // We have messages display the one up for displaying
+            this.debug('Trying to display feed ix: '+this.currentFeedIndex);
+            var noFeedsToDisplay = false;
             var dt = moment(this.currentFeed[this.currentFeedIndex].Published);
-            if (moment().diff(dt) > this.config.oldest*24*60*60*1000) this.currentFeedIndex = 0;
-
-            var msg = this.currentFeed[this.currentFeedIndex];
-
-            var tdiv = document.createElement("div");
-            tdiv.className = 'align-left';
-            var spant = document.createElement("div");
-            spant.innerHTML = moment(msg.Published).fromNow() + " "; // TODO Format the time according to how long ago it was
-            spant.className = 'dimmed xsmall';
-            tdiv.appendChild(spant);
-
-            var spanh = document.createElement("div");
-            spanh.innerHTML = msg.InfoData[0].Headline;
-            spanh.className = 'small align-left';
-            tdiv.appendChild(spanh);
-            wrapper.appendChild(tdiv);
-
-            if (this.config.showDescription) {
-                var ddiv = document.createElement("div");
-                ddiv.innerHTML = msg.InfoData[0].Description;
-                ddiv.className = 'dimmed xsmall align-left';
-                wrapper.appendChild(ddiv);
+            if (moment().diff(dt) > this.config.oldest*24*60*60*1000) {
+                noFeedsToDisplay = this.currentFeedIndex == 0;
+                this.currentFeedIndex = 0;
             }
-            var bdiv = document.createElement("div");
-            bdiv.className = 'dimmed xsmall';
-            // TODO use style instead
-            bdiv.style.marginTop = '5px';
-            bdiv.style.borderTopWidth = '1px';
-            bdiv.style.borderTopColor = '#666';
-            bdiv.style.borderTopStyle = 'dotted';
-            if (msg.InfoData[0].Area !== undefined && msg.InfoData[0].Area.length > 0) {
-                var adiv = document.createElement("span");
-                adiv.innerHTML = '<b>Area(s):</b> ';
-                for (var ia = 0 ; ia < msg.InfoData[0].Area.length; ia++) {
-                    adiv.innerHTML = adiv.innerHTML + (ia > 0 ? ', ' : '') + msg.InfoData[0].Area[ia].AreaDesc;
+            this.debug('Feed ix: '+this.currentFeedIndex + " noFeedsToDisplay: "+ noFeedsToDisplay);
+            if (noFeedsToDisplay) {
+                var div = document.createElement("div");
+                div.innerHTML = this.name + ': There are no messages younger than '+this.config.oldest + ' days';
+                //div.style.color = "red"; // TODO Change this to a custom style
+                div.className = 'dimmed xsmall';
+                wrapper.appendChild(div);
+            } else {
+                this.debug('Display feed ix: '+this.currentFeedIndex);
+
+                var msg = this.currentFeed[this.currentFeedIndex];
+
+                var tdiv = document.createElement("div");
+                tdiv.className = 'align-left';
+                var spant = document.createElement("div");
+                spant.innerHTML = moment(msg.Published).fromNow() + " " // TODO Format the time according to how long ago it was
+                    + (this.config.debug 
+                        ? moment().format('HH:mm:ss') + ' Ix:'+ this.currentFeedIndex + ' Pub: '+msg.Published 
+                        :'');
+                spant.className = 'dimmed xsmall';
+                tdiv.appendChild(spant);
+
+                var spanh = document.createElement("div");
+                spanh.innerHTML = msg.InfoData[0].Headline;
+                spanh.className = 'small align-left';
+                tdiv.appendChild(spanh);
+                wrapper.appendChild(tdiv);
+
+                if (this.config.showDescription) {
+                    var ddiv = document.createElement("div");
+                    ddiv.innerHTML = msg.InfoData[0].Description;
+                    ddiv.className = 'dimmed xsmall align-left';
+                    wrapper.appendChild(ddiv);
                 }
-                adiv.className = 'align-left';
-                adiv.style.cssFloat = 'left';
-                bdiv.appendChild(adiv);
+                var bdiv = document.createElement("div");
+                bdiv.className = 'dimmed xsmall';
+                // TODO use style instead
+                bdiv.style.marginTop = '5px';
+                bdiv.style.borderTopWidth = '1px';
+                bdiv.style.borderTopColor = '#666';
+                bdiv.style.borderTopStyle = 'dotted';
+                if (msg.InfoData[0].Area !== undefined && msg.InfoData[0].Area != null && msg.InfoData[0].Area.length > 0) {
+                    var adiv = document.createElement("span");
+                    adiv.innerHTML = '<b>Area(s):</b> ';
+                    for (var ia = 0 ; ia < msg.InfoData[0].Area.length; ia++) {
+                        adiv.innerHTML = adiv.innerHTML + (ia > 0 ? ', ' : '') + msg.InfoData[0].Area[ia].AreaDesc;
+                    }
+                    adiv.className = 'align-left';
+                    adiv.style.cssFloat = 'left';
+                    bdiv.appendChild(adiv);
+                }
+                if (this.config.debug) {
+                    var sdiv = document.createElement("span");
+                    sdiv.innerHTML = '<b>Feeds:</b> ' + this.currentFeed.length;
+                    //sdiv.className = 'align-right';
+                    //sdiv.style.cssFloat = 'right';
+                    bdiv.appendChild(sdiv);
+                }
+                if (msg.InfoData[0].SenderName !== undefined && msg.InfoData[0].SenderName != '') {
+                    var sdiv = document.createElement("span");
+                    sdiv.innerHTML = '<b>From:</b> ' + msg.InfoData[0].SenderName;
+                    sdiv.className = 'align-right';
+                    sdiv.style.cssFloat = 'right';
+                    bdiv.appendChild(sdiv);
+                }
+                wrapper.appendChild(bdiv);
             }
-            if (msg.InfoData[0].SenderName !== undefined && msg.InfoData[0].SenderName != '') {
-                var sdiv = document.createElement("span");
-                sdiv.innerHTML = '<b>From:</b> ' + msg.InfoData[0].SenderName;
-                sdiv.className = 'align-right';
-                sdiv.style.cssFloat = 'right';
-                bdiv.appendChild(sdiv);
-            }
-            wrapper.appendChild(bdiv);
         }
 
         this.currentFeedIndex++;
@@ -126,6 +151,11 @@ Module.register("MMM-CrisisInformationSweden", {
         }
 
         return wrapper;
+    },
+
+    // --------------------------------------- Debug output
+    debug: function(msg) {
+        if (this.config.debug) console.log(this.name + ': ' + msg);
     },
 
     // --------------------------------------- Handle socketnotifications
--- README.md
+++ README.md
@@ -8,7 +8,11 @@ The current feed in json format can be obtained here [http://api.krisinformation
 ## Install
 1. Clone repository into ``../modules/`` inside your MagicMirror folder.
 2. Run ``npm install`` inside ``../modules/MMM-CrisisInformationSweden/`` folder
-4. Add the module to the MagicMirror config
+3. Add the module to the MagicMirror config
+
+## Update
+1. Run ``git pull`` inside ``../modules/MMM-CrisisInformationSweden/`` folder.
+2. Run ``npm install`` inside ``../modules/MMM-CrisisInformationSweden/`` folder
 
 ## Configuration
 ```
@@ -18,15 +22,16 @@ modules: [
         module: 'MMM-CrisisInformationSweden',
         position: 'top_right'
         config: {
-            updateInterval: 10*60*1000,     // Optional. Number of ms between API updates.
+            updateInterval: 30*60*1000,     // Optional. Number of ms between API updates.
             uiUpdateInterval: 10*1000,      // Optional. Number of ms between changing to next announcement.
             alwaysNational: true,           // Optional, Regardless of other settings always show national info.
             areas: [],                      // Optional. An array of strings with area names. 
                                             // Only those messages aimed at the areas listed in the array are shown. 
                                             // The strings must match exactly with the AreaDesc of the message.
-                                            // If empty or undefined show all messages.
+                                            // If empty or undefined show all messages. Not implemented yet.
             showDescription: true,          // Optional. Show message description.
             oldest: 7,                      // Optional. Dont show messages older then this number of days.
+            debug: false                    // Optional. Enable some extra output when debugging
         }
     },
     ...
